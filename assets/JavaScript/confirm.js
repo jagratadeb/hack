533cf672321c9715af4e32aea4e2e08f5bc79292
Jagrata Deb <jagratadeb2004@gmail.com>
2024-10-13 16:45:54 +0530

    Add files via upload

---
+++ assets/JavaScript/confirm.js
@@ -0,0 +1,5 @@
+if (confirm("Are you sure that you want to be hacked? 👿") == true) {
+    alert("That button 'OK' gave me the access to your device. Now I will do what ever I wish to do👿.")
+} else {
+    alert("You have no way to cancel it. 👿 No one can save you now.")
+}
